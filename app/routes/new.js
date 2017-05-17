69ece572c88c3b4fcb0e73544d537206b8846fab
孙立刚 <17090029370@163.com>
2017-05-17 08:51:27 +0000

    redirect to original url done

--- app/routes/new.js
+++ app/routes/new.js
@@ -2,21 +2,45 @@ const express = require("express")
 const router = express.Router()
 const UrlShortener = require("../models/url-shortener")
 
-router.get(/(https?|ftp|file):\/\/[A-Za-z0-9-+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/, function (req, res, next) {
-    const originalUrl = req.path.slice(1)
-    
-    UrlShortener.findOne({original_url: originalUrl})
-      .exec((err, urlShortener) => {
+router.get(/(https?|ftp|file):\/\/[A-Za-z0-9-+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/, function(req, res, next) {
+  console.log('get 1')
+  const originalUrl = req.path.slice(1)
+
+  UrlShortener.findOne({
+      original_url: originalUrl
+    })
+    .exec((err, urlShortener) => {
+      if (err) throw err
+      if (urlShortener) return res.json(urlShortener)
+      UrlShortener.create({
+        original_url: originalUrl
+      }, (err, urlShortener) => {
         if (err) throw err
-        if (urlShortener) return res.json(urlShortener)
-        UrlShortener.create({original_url: originalUrl}, (err, urlShortener) => {
-          if (err) throw err
-          res.json(urlShortener)
-        })
+        res.json(urlShortener)
       })
+    })
 })
+
+router.get('/:shortUrl', (req, res) => {
+  console.log('get 2')
+  const shortUrl = req.params.shortUrl
+  UrlShortener.findOne({
+      short_url: shortUrl
+    })
+    .exec((err, urlShortener) => {
+      if (err) throw err
+      if (!urlShortener) return res.json({
+        error: 'URL invalid'
+      })
+      res.redirect(urlShortener.original_url)
+    })
+})
+
 router.get('/*', (req, res) => {
-  if (req.query.invalid !== 'true') res.json({error: 'URL invalid'})
+  console.log('get 3')
+  if (req.query.invalid !== 'true') res.json({
+    error: 'URL invalid'
+  })
   const originalUrl = req.path.slice(1)
   res.json({
     original_url: 'invalid',
